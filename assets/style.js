53ec20b3858a6b42f097555fbc482e66f6cafbc1
JCourtney22 <115502589+JCourtney22@users.noreply.github.com>
2022-12-16 19:19:04 +0000

    CSS and bootstrap commented out, JS file added, document ready function and api call with console log added.

---
+++ assets/style.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+    var apiURL = "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=8b6ed095ff191d87f01890c905e2502b"
+    var apiKey = "8b6ed095ff191d87f01890c905e2502b"
+console.log(apiURL)
+});
