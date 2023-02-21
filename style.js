0304acf1284748d0dd06c09e7c1529382b7f98e5
Jade <jadecourtney92@gmail.com>
2023-02-21 06:41:49 +0000

    Added variables for the different information areas.

--- style.js
+++ style.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
     var apiURL = "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=8b6ed095ff191d87f01890c905e2502b"
     var apiKey = "8b6ed095ff191d87f01890c905e2502b"
 console.log(apiURL)
+
+var getCity = document.querySelector(".city");
+var temperature = document.querySelector(".temperature");
+var icon = document.querySelector(".icon");
+var humidity = document.querySelector(".humidity");
+var wind = document.querySelector(".windSpeed");
+var searchBtn = document.querySelector(".search-button");
+var fiveDaysEl = document.querySelector(".fiveDays");
+
+
 });
